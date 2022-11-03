79629fde5c256c2b6f49c485d7b0e05e029de9b1
JosephKemper <JosephKemper@gmail.com>
2022-11-02 19:28:55 -0600

    W02 Prepare Complete

---
+++ W02 JS Operators.js
@@ -0,0 +1,25 @@
+let x = 5;
+let y = 7;
+let z = 0;
+let s = "";
+let t = true;
+// Add two numbers
+z = x + y;
+// Multiply two numbers
+z = x * y;
+// Subtract one from a number
+z = x - y;
+// concatenate two strings together.
+s = "Thank" + "You";
+// assign a value to a variable
+x = 4;
+// increment the value in a variable by 3
+x = x + 3;
+// compare two values to see if they are the same
+t = x == y;
+// check to see if one number is less than another number
+t = x < y;
+// Check to see if two values are NOT equal
+t = x != y;
+// check to see if a value is less than 10 and greater than 0
+t = (x < 10 && x > 0)
