95526942a81c5c336f57b5b643e8c491e9d6605a
JosephKemper <JosephKemper@gmail.com>
2022-11-07 19:32:07 -0700

    02 Prove work

--- W02 Prove/scripts/task2.js
+++ W02 Prove/scripts/task2.js
@@ -3,54 +3,55 @@
 /* VARIABLES */
 
 // Step 1: declare and instantiate a variable to hold your name
-let myName = "Joseph";
+const myName = "Joseph";
 
 // Step 2: place the value of the name variable into the HTML file (hint: document.querySelector())
-
+document.querySelector("#name").innerHTML = myName;
 
 // Step 3: declare and instantiate a variable to hold the current year
-
+let currentYear = 2022;
 
 // Step 4: place the value of the current year variable into the HTML file
-
+document.querySelector("#year").innerHTML = currentYear;
 
 // Step 5: declare and instantiate a variable to hold the name of your picture
-
+let picture = "images/Profile.png";
 
 // Step 6: copy your image into the "images" folder
+// Done
 
 // Step 7: place the value of the picture variable into the HTML file (hint: document.querySelector().setAttribute())
-
-
+//document.querySelector("#images/placeholder.png").setAttribute("src", picture);
+// TODO: #10 Picture Not working
 
 
 /* ARRAYS */
 
 // Step 1: declare and instantiate an array variable to hold your favorite foods
-
+let favoriteFoods = ["Broccoli Salad", " Lasagna", " Buttered Toast"];
 
 // Step 2: place the values of the favorite foods variable into the HTML file
-
+document.querySelector("#food").innerHTML = favoriteFoods;
 
 // Step 3: declare and instantiate a variable to hold another favorite food
-
+let additionalFavoriteFood = " Pizza"
 
 // Step 4: add the variable holding another favorite food to the favorite food array
-
+favoriteFoods.push(additionalFavoriteFood)
 
 // Step 5: repeat Step 2
-
+document.querySelector("#food").innerHTML = favoriteFoods;
 
 // Step 6: remove the first element in the favorite foods array
-
+favoriteFoods.shift();
 
 // Step 7: repeat Step 2
-
+document.querySelector("#food").innerHTML = favoriteFoods;
 
 // Step 8: remove the last element in the favorite foods array
-
+favoriteFoods.pop();
 
 // Step 7: repeat Step 2
-
+document.querySelector("#food").innerHTML = favoriteFoods;
 
 
