f5a01e8cb5acc2cacf73fc15535daca620541d4e
JosephKemper <JosephKemper@gmail.com>
2022-11-25 12:11:17 -0700

    Finished W04 Ponder with Functions

--- gpa.js
+++ gpa.js
@@ -1,14 +1,4 @@
-/* Display input box for user to enter grades 
-along with a button to kick off the calculation.*/
-// Completed with given code in gpa.html file
 
-/* Add an event listener to the button 
-that will do the following when clicked:*/
-
-// Get the string of grades from the input
-
-/* Convert the string to an array (String.split(',')), 
-clean up any extra spaces, and make the grades all uppercase.*/
 function getGrades (inputSelector){
     // get grades from the input box
     const grades = document.querySelector("#grades").value;
@@ -24,31 +14,44 @@ function getGrades (inputSelector){
 
 function lookupGrade(grade) {
     // converts the letter grade to it's GPA point value and returns it
+    let points = 0;
+    if (grade === "A"){
+        points = 4;
+    } else if (grade === "B") {
+        points = 3;
+    } else if (grade === "C") {
+        points = 2;
+    } else if (grade === "D") {
+        points = 1;
+    }
+    return points;
   }
 
 function calculateGpa(grades) {
     // gets a list of grades passed in
     // convert the letter grades to gpa points
+    const gradePoints = grades.map((grade) => lookupGrade(grade));
     // calculates the GPA
+    const gpa =
+        gradePoints.reduce((total, num) => total + num) / gradePoints.length;
     // return the GPA
+    return gpa.toFixed(2);
 }
 
 function outputGpa(gpa, selector) {
     // takes a gpa value and displays it in the HTML in the element identified by the selector
+    const outputElement = document.querySelector(selector)
+    outputElement.innerText = gpa;
 }
 
 function clickHandler() {
     // when the button in our html is clicked:
     // get the grades entered into the input
+    const grades = getGrades();
     // calculate the gpa from the grades entered
+    const gpa = calculateGpa(grades);
     // display the gpa
+    outputGpa(gpa, "#output");
 }
 
-
-
-// Do a lookup on each grade to convert it to it's point value (ie A = 4.0)
-
-/* Total up all the point values, 
-and divide by the number of grades to get the GPA*/
-
-// Output the GPA to the browser
+document.querySelector("#submitButton")?.addEventListener("click", clickHandler)
