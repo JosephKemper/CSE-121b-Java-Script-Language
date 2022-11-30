f1f158c59d25c90efe7d6e60a320361afe3c42d4
JosephKemper <JosephKemper@gmail.com>
2022-11-29 21:08:37 -0700

    Finished ES Modules - Ponder activities

--- modules.js
+++ modules.js
@@ -1,14 +1,16 @@
 //modules.js
 import aCourse from "./course.js";
-  
 
-  
-  document.querySelector("#enrollStudent").addEventListener("click", function () {
-    const sectionNum = document.querySelector("#sectionNumber").value;
-    aCourse.changeEnrollment(sectionNum);
-  });
-  document.querySelector("#dropStudent").addEventListener("click", function () {
-    const sectionNum = document.querySelector("#sectionNumber").value;
-    aCourse.changeEnrollment(sectionNum, false);
-  });
-  
+
+aCourse.init();
+document.querySelector( "#enrollStudent" ).addEventListener( "click", function ()
+{
+  const sectionNum = document.querySelector( "#sectionNumber" ).value;
+  aCourse.changeEnrollment( sectionNum );
+} );
+document.querySelector( "#dropStudent" ).addEventListener( "click", function ()
+{
+  const sectionNum = document.querySelector( "#sectionNumber" ).value;
+  aCourse.changeEnrollment( sectionNum, false );
+} );
+
