1ae016940eed5fb1621d001c91c537c98e71cccd
JosephKemper <JosephKemper@gmail.com>
2022-12-10 23:51:10 -0700

    Syncing work on web page.

--- W6 Prove/scripts/poems.js
+++ W6 Prove/scripts/poems.js
@@ -1,4 +1,327 @@
-// TODO: #58 Figure out how to store poems to be displayed on main page
+/*Create an array of poems with key/value pairs 
+of mood
+title
+poemText
+and dateWritten*/
+const lovePoems = [
+{
+    mood:"Hopeful and Uplifting",
+    title:"'Twas Just Two Hearts ",
+    poemText:`
+    A world of fear bore down upon them
+Alone and isolated hope was waning
+Thrown together one took a chance
+Still the light of hope refused to show its path
+Courage sparked and hope was found
 
-/* TODO: #59 Create an array of poems likely with key/value pairs 
-of date and poem */
+Slow and timid were the journey's steps
+Fear holding back the light of hope
+Through the night of fear courage held
+Till fear gave way and hope shown through
+Strength blossomed and courage grew
+
+'Twas just two hearts and love found its roots
+Beat by beat the chains of pain fall away
+Beat by beat hope shines brighter
+Beat by beat love grows deeper
+Beat by beat two hearts draw closer
+
+Beat by beat a new scene unfolds
+Hope laced through every shadow
+Joy scattered through every valley
+Courage found on every mountainside
+The sky above blanketed in love
+
+While fear tries to rule again
+Desire creeps through the two hearts
+To take this new journey
+As one takes a chance and asks
+Deanne Jensen will you marry me?`,
+    dateWritten: "14 July 2012" ,
+},
+{
+    mood:"Devoted and Humble",
+    title:"My Love",
+    poemText:`
+Will you let me cast my dreams aside to lift your heart, as your love fills mine?
+Will you continue to lift me to taste of eternity, by the touch of thy hand?
+Will you let me lift you through the eternities, to touch the glory above?
+Will you let me dance within thy gaze, and bathe within your hopes?
+Will you let me spend my strength, to fulfill your dreams?
+Will you let me spend my life, to give thee love?
+Will you let me give my heart to thee?
+Will you marry me?`,
+    dateWritten:"2 June 2013"
+},
+{
+    mood:"Humbled",
+    title:"Love for a Thousand Lives",
+    poemText:`A thousand days before
+I had just started to see your love
+A thousand days from now
+I shall long to love you
+A thousand more
+
+To live a thousand lives
+In a thousand days
+I still could ne'er hope to find
+Even a glimpse 
+Of the love we share
+
+I've asked you twice before
+And I hope to ask you
+A thousand more
+Just to see
+The love of a thousand years
+Light up as you hear
+My Love
+My Angel Queen
+Will you marry me?`,
+    dateWritten:"6 July 2014"
+},
+{
+    mood:"Humbled and Grateful",
+    title:"We Will be Three",
+    poemText:`Right now
+We are two
+But I hope to be one
+I hope our hearts will join
+And be as one
+
+Will you come with me
+Take my hand
+And dance 
+That beautiful dance
+That lets us be as one
+
+Will you marry me
+And help me forge our family
+So that we two
+May become three
+Through our new one`,
+    dateWritten:"12 July 2015"
+},
+{
+    mood:"Humbled and Grateful",
+    title:"You Are More",
+    poemText:`I know you 
+I've seen your good
+And I've seen your bad
+I've seen your ups
+And I've seen your downs
+I've seen your strengths 
+And I've seen your weaknesses 
+I've seen every part of your heart 
+And every portion of your soul 
+I know you well 
+Well enough to say
+I love every part of you 
+And I want to be a part of all of it
+Will you let me show you 
+All the joys I have seen 
+Within your soul
+Will you let me show you 
+All the strength I have found 
+Through your heart
+Will you let me share your journey 
+Through time and all eternity
+Deanne Kemper  
+Will you marry me?`,
+    dateWritten:"12 July 2016"
+},
+{
+    mood:"Humbled and Grateful",
+    title:"More Than Eternity",
+    poemText:`It's more than five years
+It's more than time
+more
+Than everything which has come before
+And more …
+than all I could ever hope to be
+Yet still
+Here is it
+… here you are
+… we are
+And what we have
+Is more than bliss
+more than joy
+more than hope
+and even more
+than love
+…
+It's an eternity
+I want to spend with you
+An eternity
+That drives me to ask
+Deanne
+My Beautiful Angel Queen
+Will you marry me?`,
+    dateWritten:"13 July 2017"
+},
+{
+    mood:"Bridled Passion",
+    title:"Love To More",
+    poemText:`Normal will not due for you
+It never has
+And it never will
+You deserve more
+So much more
+Even more than a queen
+For you are an Angel Queen
+And deserve to feel as such
+And as an Angel Queen
+So I offer you my love
+My loyalty
+My support
+And my strength
+For as long as eternity goes on
+I offer you my heart
+And everything that goes with it
+In return I simply ask
+Beg and plead
+Deanne Kemper
+Will you marry me?`,
+    dateWritten:""
+},
+{
+    mood:"Grateful",
+    title:"Contemplating Differences",
+    poemText:`I look at me now
+And think of me then…
+Then
+I look at you
+And I see
+Where the difference came
+…
+I look at heaven now
+Then think of heaven then…
+Then
+I look at you
+And I see
+Where the difference came
+…
+I look at life now
+And think of life then…
+Then 
+I look at you
+And I see
+The glorious woman
+I want to spend eternity with
+So I ask
+Deanne
+Will you marry me?`,
+    dateWritten:"14 July 2019"
+},
+{
+    mood:"Find the place where peace, hope, and love meet.",
+    title:"Eight Years into Eternity",
+    poemText:`Eight years ago
+Today
+I read you a poem
+Asking for your hand in marriage
+Asking you to join me
+In a journey that will last eternity
+
+Today
+I look back on our love
+And realize the strength behind it
+I realize the beauty of celebrating the start of our union
+On the same day we celebrate your birth
+
+Eight years ago
+I took a chance
+A chance
+That let me find eternity
+Feel heaven
+And taste of its glories
+
+So today
+I celebrate eight years
+And ask again
+Deanne, 
+My Beautiful Angel Queen
+will you marry me?`,
+    dateWritten:"14 July 2020"
+},
+{
+    mood:"Find the place where all is lost in contemplation until hope emerges",
+    title:"Life",
+    poemText:`It is not the beginning
+nor is it the end
+and the bumps along the way
+are not random events
+... 
+they are tools given by a loving God 
+given to help us learn and grow
+as we slowly work our way 
+back to Him
+...
+back to Him 
+that is my goal 
+that is my hope 
+my dream 
+...
+to get back to Him 
+we will have to work 
+we will have to pray
+and we will have to grow 
+...
+and if we am to grow
+we will need strength
+we will need each other’s help
+we will need to feel love
+...
+we will need to feel
+the love of an angel
+flowing freely
+filling every part of our life
+...
+And if I am to become more
+then I need heaven's choicest blessings
+I need someone so special
+she could only be called a Beautiful Angel Queen
+...
+So I ask you now
+Deanne
+my Beautiful Angel Queen
+will you marry me?`,
+    dateWritten:"14 July 2021"
+},
+{
+    mood:"Humbled and Grateful",
+    title:"Love Changed",
+    poemText:`You've changed my heart
+You've changed my life
+You've changed my outlook
+You've changed my goals
+And even my dreams
+There is not one part of me
+Your love has not touched
+Thanks to you
+I finally know who I am
+Who I'm meant to be
+And who I can be
+And I know
+Without you
+I'll never get there
+For you
+My Beautiful Angel Queen
+Are my hope
+My strength
+My dream
+My love
+And my taste of heaven
+So I ask you today
+Deanne
+Will you marry me?`,
+    dateWritten:"14 July 2022"
+},
+{
+    mood:"",
+    title:"",
+    poemText:``,
+    dateWritten:""
+},
+]
+
+
